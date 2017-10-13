2ca719ad5abcbb798f1f8d1739f92d6d4275385f
askipfish <541880151@qq.com>
2017-10-13 19:17:02 +0800

    删除：      objs/libev_util.a 	删除：      objs/yg_evsrv.d 	删除：      objs/yg_evsrv.o 	删除：      objs/yg_log.d 	删除：      objs/yg_log.o 	删除：      objs/yg_net.d 	删除：      objs/yg_net.o

--- objs/yg_evsrv.d
+++
@@ -1,4 +0,0 @@
-../objs/yg_evsrv.o ../objs/yg_evsrv.d : yg_evsrv.c yg_evsrv.h yg_assert.h yg_log.h \
- ../thirdparty/libevent/include/event.h \
- ../thirdparty/libevent/include/event-config.h \
- ../thirdparty/libevent/include/evutil.h yg_net.h
